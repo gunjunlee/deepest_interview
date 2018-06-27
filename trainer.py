14a149cfe7627e5301b21b799fb94a0a43854331
kevin970401 <kevin970401@gmail.com>
2018-06-27 18:08:20 +0900

    1

--- trainer.py
+++ trainer.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import os, pdb, time, copy
 from PIL import Image
+import imgaug as ia
+from imgaug import augmenters as iaa
 
 class Train():
     def __init__(self, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], input_scale_size=224, data_dir='/data/synthesized/model', model_save_name='model.ckpt', batch_size=64):
@@ -21,14 +23,12 @@ class Train():
         self.STD = std
         self.data_transforms = {
                                     'train': transforms.Compose([
-                                        transforms.Lambda(lambda img: self.convert_train(img)),
+                                        transforms.RandomCrop(32, padding=4),
                                         transforms.RandomHorizontalFlip(),
-                                        transforms.RandomVerticalFlip(),
                                         transforms.ToTensor(),
                                         transforms.Normalize(self.MEAN, self.STD)
                                     ]),
                                     'test': transforms.Compose([
-                                        transforms.Lambda(lambda img: self.convert(img)),
                                         transforms.ToTensor(),
                                         transforms.Normalize(self.MEAN, self.STD)
                                     ])
@@ -38,10 +38,23 @@ class Train():
         self.dataloaders = {x: torch.utils.data.DataLoader(self.image_datasets[x], batch_size=self.BATCH_SIZE, shuffle=True, num_workers=4) for x in ['train', 'test']}
         self.dataset_sizes = {x: len(self.image_datasets[x]) for x in ['train', 'test']}
         self.class_names = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        self.iaa_seq = iaa.Sequential([
+            iaa.PerspectiveTransform(scale=0.050),
+            iaa.Multiply((0.8, 1.2)),
+            iaa.Affine(
+                scale={"x":(1, 1.1), "y":(1, 1.1)}
+            ),
+            iaa.SaltAndPepper((0.05)),
+            iaa.Add((-20, 20)),
+            iaa.GaussianBlur((0, 0.50))
+        ])
 
     def convert_train(self, img):
         if img.mode != 'RGB':
             img = img.convert('RGB')
+        if np.random.randint(2) == 0:    
+            img = self.iaa_seq.augment_image(np.array(img))
+            img = Image.fromarray(img)
         # img = img.resize((self.IMAGE_SCALE_SIZE, self.IMAGE_SCALE_SIZE))
         return img
 
@@ -103,7 +116,7 @@ class Train():
                     epoch_acc_1 =  running_corrects_1.item()/num_label_1.item()
                 except:
                     pdb.set_trace()
-                print('{} Loss: {:.4f} Acc: {:.4f} {} Acc: {:.4f} {} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc, self.class_names[0], epoch_acc_0, self.class_names[1], epoch_acc_1))
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc, self.class_names[0], epoch_acc_0, self.class_names[1], epoch_acc_1))
 
                 if phase == 'test' and epoch_acc > best_acc:
                     best_acc = epoch_acc
@@ -119,6 +132,8 @@ class Train():
 
         return model
 
+    def load_model(self, model, ckpt_name):
+        model.load_state_dict(torch.load(os.path.join('ckpt', ckpt_name)))
 
 if __name__ == '__main__':
     pdb.set_trace()
